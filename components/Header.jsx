9766fc0d50d7c0feba7ec75fd341d999325dfc3a
Iamozart2003 <codesingh28@gmail.com>
2023-06-08 01:27:00 +0530

    Hum First

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between px-4 pt-4'>
+      <h2>Good Evening Boss</h2>
+      <h2>Hello Mr.Singh</h2>
+    </div>
+  )
+}
+
+export default Header
